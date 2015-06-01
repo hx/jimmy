dee06333d30af49d79989c3aa351596cba091c6c
Neil E. Pearson <neil.pearson@orionvm.com>
2015-06-01 13:20:18 +1000

    Reworked domain schema import methods

--- lib/jimmy/domain.rb
+++ lib/jimmy/domain.rb
@@ -21,8 +21,16 @@ module Jimmy
 
     def import(path)
       path = Pathname(path) unless path.is_a? Pathname
-      @types = import_schemas(path + 'types', path).map { |k, v| [k.to_sym, v] }.to_h
-      @schemas = import_schemas(path, path, 'types/')
+
+      types_path = path + 'types'
+      glob types_path, path do |name, schema|
+        @types[name.to_sym] = schema
+      end
+
+      glob path do |name, schema|
+        next if name =~ %r`^(types|partials)/`
+        @schemas[name] = schema
+      end
     end
 
     def [](schema_name)
@@ -38,18 +46,19 @@ module Jimmy
 
     private
 
-    def import_schemas(path, base_path, reject_prefix = nil)
-      result = {}
+    def glob(path, base_path = nil, &block)
+      base_path ||= path
       Dir[path + '**/*.rb'].each do |full_path|
         full_path = Pathname(full_path)
         relative_path = full_path.relative_path_from(path)
-        next if reject_prefix && relative_path.to_s.start_with?(reject_prefix)
-        base_name = relative_path.to_s[0..-4]
-        schema = instance_eval(full_path.read, full_path.to_s).schema
-        schema.name = full_path.relative_path_from(base_path).to_s[0..-4]
-        result[base_name] = schema
+        args = [relative_path.to_s[0..-4]]
+        if block.arity == 2
+          schema = instance_eval(full_path.read, full_path.to_s).schema
+          schema.name = full_path.relative_path_from(base_path).to_s[0..-4]
+          args << schema
+        end
+        yield *args
       end
-      result
     end
 
     def export_schema(schema, target_path)
